906abfa72b4100eb0866ae553405ed30099e2195
Zalbani <30436159+zalbani@users.noreply.github.com>
2023-01-18 21:00:34 +0100

    Add mint button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import myEpicNft from './utils/MyEpicNFT.json'
 import './styles/App.css';
 
 // Constants
@@ -69,11 +71,44 @@ const App = () => {
       }
     }
 
+    const askContractToMintNft = async () => {
+      const CONTRACT_ADDRESS = "0x9daacFbc5E0819331421cE70652226DaD850D501";
+    
+      try {
+        const { ethereum } = window;
+    
+        if (ethereum) {
+          const provider = new ethers.providers.Web3Provider(ethereum);
+          const signer = provider.getSigner();
+          const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+    
+          console.log("Going to pop wallet now to pay gas...")
+          let nftTxn = await connectedContract.makeAnEpicNFT();
+    
+          console.log("Mining...please wait.")
+          await nftTxn.wait();
+          
+          console.log(`Mined, see transaction: https://goerli.etherscan.io/tx/${nftTxn.hash}`);
+    
+        } else {
+          console.log("Ethereum object doesn't exist!");
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
   // Render Methods
   const renderNotConnectedContainer = () => (
     <button onClick={connectWallet} className="cta-button connect-wallet-button">
       Connect to Wallet
     </button>
+  );
+   
+  const renderMintButton = () => (
+   <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
+     Mint NFT
+   </button>
   );
 
   useEffect(() => {
@@ -88,7 +123,7 @@ const App = () => {
           <p className="sub-text">
             Each unique. Each beautiful. Discover your NFT today.
           </p>
-          {renderNotConnectedContainer()}
+          {currentAccount === "" ? renderNotConnectedContainer() : renderMintButton()}
         </div>
       </div>
     </div>
