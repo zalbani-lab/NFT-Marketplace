f058089fd772c720b2415f338c3c78cd49330065
Zalbani <30436159+zalbani@users.noreply.github.com>
2023-01-19 19:52:34 +0100

    Listen for NFT minted event

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import './styles/App.css';
 const OPENSEA_LINK = '';
 const TOTAL_MINT_COUNT = 50;
 
+const CONTRACT_ADDRESS = "0xE324D36201550A5B1dB756cA0f104E8028817fBD";
+
 const App = () => {
 
   /*
@@ -42,6 +44,7 @@ const App = () => {
     } else {
       console.log("No authorized account found");
     }
+    setupEventListener();
   }
   
   /*
@@ -65,14 +68,44 @@ const App = () => {
         * Boom! This should print out public address once we authorize Metamask.
         */
         console.log("Connected", accounts[0]);
-        setCurrentAccount(accounts[0]); 
+        setCurrentAccount(accounts[0]);
+        setupEventListener() 
       } catch (error) {
         console.log(error);
       }
     }
 
+  // Setup our listener.
+  const setupEventListener = async () => {
+    // Most of this looks the same as our function askContractToMintNft
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        // Same stuff again
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+
+        // THIS IS THE MAGIC SAUCE.
+        // This will essentially "capture" our event when our contract throws it.
+        // If you're familiar with webhooks, it's very similar to that!
+        connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber())
+          alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
+        });
+
+        console.log("Setup event listener!")
+
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
     const askContractToMintNft = async () => {
-      const CONTRACT_ADDRESS = "0x9daacFbc5E0819331421cE70652226DaD850D501";
     
       try {
         const { ethereum } = window;
